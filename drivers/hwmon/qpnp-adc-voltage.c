07c974da040a33c5233b20522d893dffb6fc0694
Jacob Whatley <jacob.whatley91@gmail.com>
2017-07-26 22:45:44 +0000

    power: add changes from N108
    - reference https://github.com/LineageOS/android_kernel_nextbit_msm8992
        - remove BBox logging
        - remove FIH rebootreason
    
    Change-Id: I114d7787d430a195901467fa1428859ed539ab00

--- drivers/hwmon/qpnp-adc-voltage.c
+++ drivers/hwmon/qpnp-adc-voltage.c
@@ -182,6 +182,8 @@ static struct qpnp_vadc_rscale_fn adc_vadc_rscale_fn[] = {
 	[SCALE_RVADC_ABSOLUTE] = {qpnp_vadc_absolute_rthr},
 };
 
+struct qpnp_vadc_chip *fih_vadc = NULL;
+
 static int32_t qpnp_vadc_read_reg(struct qpnp_vadc_chip *vadc, int16_t reg,
 								u8 *data)
 {
@@ -2118,6 +2120,22 @@ int32_t qpnp_vadc_end_channel_monitor(struct qpnp_vadc_chip *chip)
 }
 EXPORT_SYMBOL(qpnp_vadc_end_channel_monitor);
 
+int fih_get_usbin_voltage_now(void)
+{
+	int rc = -1;
+	struct qpnp_vadc_result result;
+
+	rc = qpnp_vadc_read(fih_vadc, 0, &result);
+
+	if (rc) {
+		pr_err("FIH VADC read error with %d\n", rc);
+		return 0;
+	}
+
+	return result.physical;
+}
+EXPORT_SYMBOL(fih_get_usbin_voltage_now);
+
 static ssize_t qpnp_adc_show(struct device *dev,
 			struct device_attribute *devattr, char *buf)
 {
@@ -2402,6 +2420,9 @@ static int qpnp_vadc_probe(struct spmi_device *spmi)
 	}
 
 	vadc->vadc_iadc_sync_lock = false;
+
+	fih_vadc = vadc;
+
 	dev_set_drvdata(&spmi->dev, vadc);
 	list_add(&vadc->list, &qpnp_vadc_device_list);
 
